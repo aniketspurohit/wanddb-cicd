2909b25e5014c94a7a7c3372a5cc94bf955350cb
Aniket S Purohit <aniket.purohit35@gmail.com>
2023-03-23 19:24:04 +0000

    fix: print statement fix
    in main

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import os
 
 if __name__=="__main__":
   if (my_name:=os.getenv("MY_NAME")):
-    print(f"wandb's version: {wandb.__version__} - len(my_name)")
+    print(f"wandb's version: {wandb.__version__} - {len(my_name)}")
